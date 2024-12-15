692f6be012c944d3a2cf3eb195a168716aad5696
josma04 <mauricio.unda09@gmail.com>
2024-12-15 12:21:59 -0500

    Update asgi.py

--- miproyecto/asgi.py
+++ miproyecto/asgi.py
@@ -1,16 +1,12 @@
-"""
-ASGI config for miproyecto project.
+import os 
+import sys
+path = os.path.expanduser('~/Mi-amor') 
 
-It exposes the ASGI callable as a module-level variable named ``application``.
+if path not in sys.path:
+    sys.path.insert(0, path)
+    os.environ[ 'DJANGO_SETTINGS_MODULE'] = 'miproyecto.settings' 
+    
 
-For more information on this file, see
-https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
-"""
-
-import os
-
-from django.core.asgi import get_asgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'miproyecto.settings')
-
-application = get_asgi_application()
+from django.core.wsgi import get_wsgi_application
+from django.contrib.staticfiles.handlers import StaticFilesHandler 
+application = StaticFilesHandler(get_wsgi_application())
